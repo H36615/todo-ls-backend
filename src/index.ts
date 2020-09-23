ca6db62e2f881684e79233c42a6db8a785e0fc0e
H36615 <h36615@protonmail.com>
2020-09-23 15:50:00 +0300

    Fix loggers

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,13 @@ import express from 'express';
 const port = 3000;
 const app = express();
 
+console.log("starting server...");
+
+
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
 app.listen(port, () => {
     console.log("running on port " + port);
-    
 });
-
-console.log("Program has ended. ");
