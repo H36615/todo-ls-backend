825eb0a87a4c34df7061889baba730ef2559dbea
H36615 <h36615@protonmail.com>
2020-09-27 14:41:59 +0300

    Fix migrate script, create rollback-last & seed scripts for todo-item table

--- db/migrations/20200924163320_todo.ts
+++ db/migrations/20200924163320_todo.ts
@@ -2,6 +2,7 @@
 // Base created with 'npx knex migrate:make <table name>'
 
 import * as Knex from "knex";
+import { todoItemDBModel, TodoItemStatus } from "../../src/models/todo-item/todo-item";
 
 const todoItemTable = "todo_item";
 
@@ -9,13 +10,19 @@ const todoItemTable = "todo_item";
 export async function up(knex: Knex): Promise<void> {
 	return knex.schema.createTable(todoItemTable, tbl => {
 		tbl.increments();
-		tbl.text("task").notNullable();
-		tbl.enum("state", ["todo", "in-progress", "done"]).notNullable();
+		tbl.text(todoItemDBModel.columns.task).notNullable();
+		tbl.enum(
+			todoItemDBModel.columns.status,
+			Object.values(TodoItemStatus),
+			{ useNative: true, enumName: todoItemDBModel.columns.status }
+		).notNullable();
 	});
 }
 
 
 export async function down(knex: Knex): Promise<void> {
 	knex.schema.dropTableIfExists(todoItemTable);
+
+	// TODO enum types seem not to be affected?
 }
 
