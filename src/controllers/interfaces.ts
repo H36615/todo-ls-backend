73e476be4f8df314c797e0f923037b1fa7a2f1bf
H36615 <h36615@protonmail.com>
2020-10-04 17:50:10 +0300

    Functionalize controller functions

--- src/controllers/interfaces.ts
+++ src/controllers/interfaces.ts
@@ -2,5 +2,5 @@ import { Request, Response } from "express";
 
 
 export interface IController {
-	(req: Request, res: Response): void
+	(req: Request, res: Response): Promise<void>
 }
--- src/controllers/todo-item/todo-item.ts
+++ src/controllers/todo-item/todo-item.ts
@@ -2,8 +2,8 @@ import dbConfig from "../../config/db-config";
 import { todoItemDBModel } from "../../models/todo-item/todo-item";
 import { IController } from "../interfaces";
 
-export const getAllTodoItems: IController = (req, res): void => {
-	dbConfig(todoItemDBModel.table).select("*").then(rows => {
+export const getAllTodoItems: IController = (req, res): Promise<void> => {
+	return dbConfig(todoItemDBModel.table).select("*").then(rows => {
 		res.send(rows);
 	});
 };
