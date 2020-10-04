6a9106039955c89f27f7a3de75e04e2757e0d3ae
H36615 <h36615@protonmail.com>
2020-10-04 20:43:19 +0300

    Add knex testing framwerk to project & create 1st test

---
+++ src/controllers/todo-item/todo-item.test.ts
@@ -0,0 +1,47 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import mockKnex from "mock-knex";
+import dbConfig from "../../config/db-config";
+import { ITodoItem, TodoItemStatus } from "../../models/todo-item/todo-item";
+import { getAllTodoItems } from "./todo-item";
+
+mockKnex.mock(dbConfig);
+
+const tracker = mockKnex.getTracker();
+const responseMock: ITodoItem[] = [
+	{
+		id: 10,
+		status: TodoItemStatus.inProgres,
+		task: "do a thing",
+		userId: 20,
+	},
+	{
+		id: 11,
+		status: TodoItemStatus.delayed,
+		task: "make that",
+		userId: 21,
+	}
+];
+
+test("getAllTodoItems should respond w/ correct value(s)", async (done) => {
+	// Arrange
+	tracker.install();
+	tracker.on(
+		"query",
+		(query) => { query.response(responseMock); }
+	);
+	const mockRes: any = {
+		send: jest.fn(),
+	};
+
+	// Act
+	getAllTodoItems({} as any, mockRes).then(() => {
+
+		// Assert
+		expect(mockRes.send).toHaveBeenCalledWith(responseMock);
+
+		done();
+	});
+});
+
+tracker.uninstall();
