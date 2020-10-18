4b07e9d488c3107b6289aa78c5496a623f53d326
H36615 <h36615@protonmail.com>
2020-10-18 13:47:05 +0300

    Add health route tests

--- src/api/routes/health/health.test.ts
+++ src/api/routes/health/health.test.ts
@@ -1,4 +1,32 @@
 
-test("test route", () => {
-	throw new Error("Not implemented");
+import { mockExpress } from "../../../../testing/express-mocks";
+
+const healthRoute = "/health";
+describe(healthRoute, () => {
+
+	test("no spy should be called without initialization", () => {
+	
+		// Arrange
+		const getSpy = jest.fn();
+		mockExpress(getSpy);
+	
+		// Assert
+		expect(getSpy).not.toHaveBeenCalled();
+	});
+
+	test("route should be initialized", () => {
+
+		// Arrange
+		const getSpy = jest.fn();
+		mockExpress(getSpy);
+	
+		// Act
+		require("./health");
+	
+		// Assert
+		expect(getSpy).toHaveBeenCalledWith(
+			healthRoute,
+			expect.any(Function), // Not really easy way to test func param.
+		);
+	});
 });
