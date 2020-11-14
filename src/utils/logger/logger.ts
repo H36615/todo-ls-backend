0d47c9a1b1cfca5e93581370103a28c08189101d
H36615 <h36615@protonmail.com>
2020-11-14 16:37:14 +0200

    Silence linter errors since no implementation exists yet

--- src/utils/logger/logger.ts
+++ src/utils/logger/logger.ts
@@ -1,10 +1,15 @@
 
 export class Logger {
+
+	// TODO remove eslint disabler when implemented.
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	public static error(message: string): void {
 		// TODO implement.
 		// console.log(errorMessage);
 	}
 
+	// TODO remove eslint disabler when implemented.
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	public static debug(message: string): void {
 		// TODO implement.
 		// console.log(message);
