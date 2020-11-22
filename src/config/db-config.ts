657484d70f36d6af1f47e07fa38c00753d1498a5
H36615 <h36615@protonmail.com>
2020-11-22 20:44:36 +0200

    Use .envified environment value

--- src/config/db-config.ts
+++ src/config/db-config.ts
@@ -2,8 +2,15 @@
 
 import knex, { Config } from "knex";
 import knexFile from "../../knexfile";
+import { Environment, EnvironmentUtils } from "../utils/environment/environment";
 
-const environment = "development"; // TODO || .envify process.env
-const configOptions: Config = knexFile[environment] as Config;
+const environment = EnvironmentUtils.getValidatedEnvironment();
+
+let configOptions: Config;
+if (environment === Environment.development)
+	configOptions = knexFile[environment] as Config;
+else {
+	throw new Error("implementation missing for environment value");
+}
 
 export const dBConfig = knex(configOptions);
